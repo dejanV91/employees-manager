5179bd6cced12ae686117dd3a84bf4cb2c5ce902
dejanV91 <dejanvukoicic91@gmail.com>
2023-03-09 18:49:38 +0100

    finish create pages folder

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,10 @@ import React from "react";
 import "./styles/App.scss";
 
 import HomePage from "./pages/HomePage";
-// import Header from "./components/Header/Header";
-// import EmployeesList from "./components/EmployeesList";
 
 function App() {
   return (
     <div className="App">
-      {/* <Header></Header>
-      <EmployeesList></EmployeesList> */}
       <HomePage></HomePage>
     </div>
   );
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import Header from "../components/Header/Header";
-import Employee from "../components/EmployeesList";
 import EmployeesList from "../components/EmployeesList";
 
 const HomePage = () => {
-  <>
-    <Header />
-    <EmployeesList />
-  </>;
+  return (
+    <>
+      <Header></Header>
+      <EmployeesList></EmployeesList>
+    </>
+  );
 };
 
 export default HomePage;

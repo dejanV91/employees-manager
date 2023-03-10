ea094b951f664f9bd4a74e369d1799bd37c261e8
dejanV91 <dejanvukoicic91@gmail.com>
2023-03-10 12:50:14 +0100

    delete buttons group

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -1,12 +1,19 @@
 import React from "react";
-import ButtonsGroup from "./Buttons/ButtonsGroup";
+
+import Delete from "./Buttons/Delete";
+import ReadMore from "./Buttons/ReadMore";
+import Update from "./Buttons/Update";
 
 const Item = ({ name }) => {
   return (
     <div>
       <div className="item">
         <h3>{name}</h3>
-        <ButtonsGroup></ButtonsGroup>
+        <div className="buttons-group">
+          <ReadMore />
+          <Update />
+          <Delete />
+        </div>
       </div>
       <div className="dropDown">
         <h2>Full Name:</h2>
